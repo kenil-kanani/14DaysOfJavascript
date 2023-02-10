9f0af8413036f09cd9159ee1d7c5602c67a6b0bf
Kenil Kanani <104387424+kenil-kanani@users.noreply.github.com>
2023-02-10 19:33:19 +0530

    Added Project Digital Clock

---
+++ Digital Clock./index.js
@@ -0,0 +1,29 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const ampm = document.getElementById("ampm");
+
+function updatetime() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h > 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hour.innerText = h;
+    minute.innerText = m;
+    second.innerText = s;
+    ampm.innerText = ampm;
+    setTimeout(() => {
+        updatetime();
+    }, 1000);
+}
+updatetime();
